b7ea4581ca6224ee7c8ae5d46106f02054fe15fe
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-03-02 14:23:56 -0300

    Ajuste em cores e integração de Icones

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import {
+  AiOutlineInstagram,
+  AiFillGithub,
+  AiOutlineLinkedin
+} from 'react-icons/ai'
+
 export default function Home() {
   return (
     <body>
@@ -21,13 +27,19 @@ export default function Home() {
                   href="https://www.linkedin.com/in/kaio-rodrigo-8392a421a/"
                   target="_blank"
                 >
-                  <i class="fab fa-linkedin">In.</i>
+                  <span>
+                    <AiOutlineInstagram className="iconSocial" />
+                  </span>
                 </a>
                 <a href="https://github.com/KaioRodrigoDev" target="_blank">
-                  <i class="fab fa-github">In.</i>
+                  <span>
+                    <AiFillGithub className="iconSocial" />
+                  </span>
                 </a>
                 <a href="https://instagram.com/kaio_dev" target="_blank">
-                  <i class="fab fa-instagram">In.</i>
+                  <span>
+                    <AiOutlineLinkedin className="iconSocial" />
+                  </span>
                 </a>
               </div>
               <br />
@@ -42,17 +54,17 @@ export default function Home() {
           </div>
           <br />
           <a
-            class="btn btnPrimary"
+            className="btn btnPrimary"
             target="blank"
             href="https://drive.google.com/file/d/1w8z5AxvtZi-cGld526KhBsfMxeqdTRpf/view?usp=sharing"
           >
             Curriculum
           </a>
-          <a class="btn btnSecont" href="/projects">
+          <a className="btn btnSecont" href="/projects">
             Projetos
           </a>
-          <hr class="linha" />
-          <h2 class="ido title">Sobre mim</h2>
+          <hr className="linha" />
+          <h2 className="ido title">Sobre mim</h2>
           <br />
           <p>
             Sou desenvolvedor de web e mobile focado no front-end. Tenho 18 anos
@@ -67,7 +79,7 @@ export default function Home() {
           </p>
           <br />
           <section>
-            <div class="boxes">
+            <div className="boxes">
               <h3 className="title">PHP</h3>
               <br />
               <p>
@@ -75,7 +87,7 @@ export default function Home() {
                 desenvolvimento Web.
               </p>
             </div>
-            <div class="boxes">
+            <div className="boxes">
               <h3 className="title">JavaScript</h3>
               <br />
               <p>
@@ -83,7 +95,7 @@ export default function Home() {
                 junto com HTML e CSS
               </p>
             </div>
-            <div class="boxes">
+            <div className="boxes">
               <h3 className="title">React</h3>
               <br />
               <p>
@@ -91,7 +103,7 @@ export default function Home() {
                 Mobile. Dando assim uma grande liberdade para seu uso.
               </p>
             </div>
-            <div class="boxes">
+            <div className="boxes">
               <h3 className="title">HTML e CSS</h3>
               <br />
               <p>
