34a9261259376e20423fee4ad9dc9d340b9e982c
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-05-22 02:47:13 -0300

    Adicionando Prismic

---
+++ .slicemachine/assets/slices/Project/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Project';
+
+export default {
+  title: 'slices/Project'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"project","items":[],"primary":{"Numero":6777},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ .slicemachine/assets/slices/Test/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Test';
+
+export default {
+  title: 'slices/Test'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"test","items":[],"primary":{"title":[{"type":"heading1","text":"Strategize strategic architectures","spans":[]}],"description":[{"type":"paragraph","text":"Elit incididunt nisi consequat quis mollit sit est est veniam cupidatat et ad sunt ipsum occaecat. Est amet enim aliquip labore ipsum elit nostrud non commodo deserunt sunt.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
